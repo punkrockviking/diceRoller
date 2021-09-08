c992ec749d6c64c9c3a40a3b2834aec2250bf860
punkrockviking <danjeccles@gmail.com>
2021-09-08 12:05:32 -0600

    (feat) manipulating data

--- index.js
+++ index.js
@@ -19,11 +19,32 @@ const Movie = mongoose.model('Movie', movieSchema);
 // const inception = new Movie({title: 'Inception', year: 2000, score: 7, rating: 'PG-13'})
 // inception.save()
 
-Movie.findOneAndUpdate({title: 'Inception'}, {score: 6}, (err) => {
-  if(err) {
-    console.log(err)
-  }
-})
+// Movie.findOneAndUpdate({title: 'Inception'}, {score: 6}, (err) => {
+//   if(err) {
+//     console.log(err)
+//   }
+// })
+
+// Movie.insertMany([
+//   { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+//   { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+//   { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+//   { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+//   { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+// ])
+// .then(data => {
+//   console.log('Inserted movies!')
+//   console.log(data)
+// })
+
+
+
+
+
+
+
+
+// TO TEST CONNECTION
 
 // const userSchema = require('./userSchema.js')
 // const User = mongoose.model('user', userSchema, 'user')
