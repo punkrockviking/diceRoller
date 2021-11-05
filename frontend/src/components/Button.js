f2d4221ed8fd9af96650646be8b2dfca076f07b9
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2021-11-04 22:10:02 -0600

    (feat) change style button component file name

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    width: 200px;
+    height: 50px;
+    background-color: ${(props) => props.backgroundColor};
+    &:hover {
+        background-color: ${(props) => props.hoverColor};
+        cursor: pointer;
+    }
+    &:focus {
+        background-color: ${(props) => props.focusColor};
+    }
+`
+
+
+export default Button
